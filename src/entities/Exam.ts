8fe502be48f5239a71e136f134e6cafa20cc8a9c
Paulo Moss <paulo.moss@poli.ufrj.br>
2021-09-01 10:02:13 -0300

    Alter Exam Entity Column to Select

--- src/entities/Exam.ts
+++ src/entities/Exam.ts
@@ -1,40 +1,45 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, OneToMany } from "typeorm";
-import Subject from './Subject';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  OneToMany,
+} from "typeorm";
+import Subject from "./Subject";
 import Category from "./Category";
 import Teacher from "./Teacher";
 import Semester from "./Semester";
 
-
 @Entity("exam")
 export default class Exam {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column()
+  link: string;
 
-    @Column()
-    link: string;
+  @Column({ select: false })
+  subjectId: number;
 
-    @Column({ select: false })
-    subjectId: number;
+  @Column({ select: false })
+  teacherId: number;
 
-    @Column({ select: false })
-    teacherId: number;
+  @Column({ select: false })
+  semesterId: number;
 
-    @Column({ select: false })
-    semesterId: number;
+  @Column()
+  categoryId: number;
 
-    @Column({ select: false })
-    categoryId: number;
+  @ManyToOne(() => Subject, (subject) => subject.exams, { cascade: true })
+  subject: Subject;
 
-    @ManyToOne(() => Subject, subject => subject.exams, {cascade: true})
-    subject: Subject;
+  @ManyToOne(() => Teacher, (teacher) => teacher.exams, { cascade: true })
+  teacher: Teacher;
 
-    @ManyToOne(() => Teacher, teacher => teacher.exams, { cascade: true})
-    teacher: Teacher;
+  @ManyToOne(() => Semester, (semester) => semester.exams, { cascade: true })
+  semester: Semester;
 
-    @ManyToOne(() => Semester, semester => semester.exams, { cascade: true})
-    semester: Semester;
-
-    @ManyToOne(() => Category, category => category.exams, { cascade: true})
-    category: Category;
+  @ManyToOne(() => Category, (category) => category.exams, { cascade: true })
+  category: Category;
 }
